6399addbd79a76962761c7b5bc44addb6ac7c28b
Naresh-b17 <naresh.boyini@opstree.com>
2026-01-15 00:23:03 +0530

    Add src_dir and ami_name variables to build script

--- build.sh
+++ build.sh
@@ -64,6 +64,12 @@ logInfoMessage "Performing action: ${ACTION}"
       -var subnet_id='${SUBNET_ID}' \
       -var security_group_id='${SECURITY_GROUP_ID}' \
       -var app_dir='${APP_DIR:-/var/www/html}' \
+      -var src_dir='${SRC_DIR:-/tmp/app}' \
+      -var ami_name='${AMI_NAME:-app}' \
+      -var instance_type='${INSTANCE_TYPE:-t3.micro}' \
+      -var iam_role='${IAM_ROLE:-}' \
+      -var root_volume_size='${ROOT_VOLUME_SIZE:-8}' \
+      -var root_volume_type='${ROOT_VOLUME_TYPE:-gp3}' \
       ${PACKER_DIR}"
 
     # Optional runtime commands (passed as single string)
