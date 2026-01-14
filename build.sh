8fc38cdf5130bcd7292ad382dfd0444c23bb8898
Naresh-b17 <naresh.boyini@opstree.com>
2026-01-14 20:48:58 +0530

    Implement IAM role assumption in build script
    Added functionality to assume an IAM role if enabled, including error handling and environment variable exports.

--- build.sh
+++ build.sh
@@ -21,6 +21,24 @@ logInfoMessage "Processing codebase at [${CODEBASE_LOCATION}]"
 
 cd "${CODEBASE_LOCATION}"
 
+# Assume role if enabled
+if [ "$ASSUME_OTHER_ROLE" == "true" ]; then
+    logInfoMessage "Assuming IAM role..."
+
+    role_output=$(aws sts assume-role \
+        --role-arn arn:aws:iam::$ACCOUNT_ID:role/$ROLE_NAME \
+        --role-session-name "$ROLE_SESSION_NAME")
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to assume role."
+        exit 1
+    fi
+
+    export AWS_ACCESS_KEY_ID=$(echo "$role_output" | jq -r '.Credentials.AccessKeyId')
+    export AWS_SECRET_ACCESS_KEY=$(echo "$role_output" | jq -r '.Credentials.SecretAccessKey')
+    export AWS_SESSION_TOKEN=$(echo "$role_output" | jq -r '.Credentials.SessionToken')
+fi
+
 logInfoMessage "Performing action: ${ACTION}"
 
 # ----------------------------------------
